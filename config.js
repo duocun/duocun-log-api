94d8ab59bce978f2d12b55879476e19411f76be4
zlkca <likzhang@gmail.com>
2020-08-13 18:41:27 -0400

    Add env file

--- config.js
+++ config.js
@@ -7,6 +7,9 @@ const env = process.env;
 export const config = {
     DB_CONN: `mongodb://${env.DB_USERNAME}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?authSource=${env.AUTH_DB_NAME}`,
     DB_HOST: env.DB_HOST,
-    DB_PORT: env.DB_PORT,
-    PORT: 8003
+    DB_NAME: 'duocun-logs',
+    DB_PORT: '27017',
+    DB_POOL_SIZE: 10,
+    DB_USERNAME:'duocun',
+    PORT: 8003,
 }
